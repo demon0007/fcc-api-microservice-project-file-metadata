ff15c17d11e613fc9f9be06d107e6fabdf54e432
Glitch (thorn-tune) <none>
2019-01-23 18:22:13 +0000

    🛤🎻 Checkpoint ./server.js:2619925/264

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
+app.post('/api/fileanalyse',upload.single('upfile'), (req, res) => {
+  // console.log(req.file)
+  res.json({"name": req.file.originalname, type:req.file.mimetype, size:req.file.size})
+})
+
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
